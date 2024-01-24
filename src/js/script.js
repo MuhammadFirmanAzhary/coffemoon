30ec0bc455ed9829ebee9413efb9baa368118009
muhammadfirmanazhary <muhammadfirmanazhary.com>
2024-01-24 14:21:19 +0700

    update daekmode

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,14 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const navbarFixed = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > navbarFixed){
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
     }else{
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
     }
 }   
 
@@ -21,4 +24,32 @@ const navMenu =document.querySelector('#nav-menu');
     
  });
 
-   
+//klik dimanapun
+window.addEventListener('click', function(a){
+ if (a.target !=humberger && a.target !=navMenu){
+    humberger.classList.remove('active');
+    navMenu.classList.add('hidden');
+ }
+});
+
+// darkMode
+const darkToggle=document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click',function(){
+ if (darkToggle.checked){
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+ }else{
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+
+ }
+});
+
+//perpindahan tombol
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html'],
-  darkMode:['ary '],
+  darkMode:['class'],
   theme: {
     extend: {
       colors:{
